4772ada44a37dac129deeedf842d969e5ac49d44
Thomas <thomasdeutschpro@gmail.com>
2023-06-15 14:44:50 -0700

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * * * main - void Programm
+ * *  * Return: success
+ */
+
+int main(void)
+
+{
+	char charType;
+	int intType;
+	long int longintType;
+	long long int longlongintType;
+	float floatType;
+
+
+
+	printf("Size of char: %zu byte\n", sizeof(charType));
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of long int: %zu bytes\n", sizeof(longintType));
+	printf("Size of long long int: %zu bytes\n", sizeof(longlongintType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+
+	return (0);
+}
