33e36eff0e7688f2ea99b900b6158c194398bd46
Thomas <thomasdeutschpro@gmail.com>
2023-06-15 13:15:26 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+/**
+ *          * main - Coucou
+ *          * return - 0 good job
+*/
 int main(void)
-	/**
-	 * main - Entree
-	 * return - 0
-	 */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
