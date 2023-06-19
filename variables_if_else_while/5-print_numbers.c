ddc7e538eaa5e15d17ebfb6d504506caebc9d54e
Thomas <thomasdeutschpro@gmail.com>
2023-06-19 06:04:01 -0700

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *  *  *  * main - void
+ *   *   * Return: Always 0 (Success)
+ *    *    **/
+
+int main(void)
+{
+	int number;
+
+	number = 0;
+
+
+for (number = 0; number <= 10; number++)
+{
+
+	printf("%d", number);
+	return (0);
+}
+}
