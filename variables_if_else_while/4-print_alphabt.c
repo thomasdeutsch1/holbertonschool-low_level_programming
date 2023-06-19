31d7d76f1678cb1f8678fc3051cf9102716fa078
Thomas <thomasdeutschpro@gmail.com>
2023-06-19 05:32:50 -0700

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,16 +10,17 @@ int main(void)
 	char lowercase;
 
 	lowercase = 'a';
-	
 
-	while (lowercase <= 'z'){
-	if (lowercase != 'e' && lowercase != 'q')
-	{putchar(lowercase); }
+
+	while (lowercase <= 'z')
+	{
+		if (lowercase != 'e' && lowercase != 'q')
+		{putchar(lowercase); }
 		lowercase++;
-	
+
 	}
 
 
 	putchar('\n');
 	return (0);
-}              
+}
