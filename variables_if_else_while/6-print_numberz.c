18971231cab4895cdb0298d460539c5152cb0bc8
Thomas <thomasdeutschpro@gmail.com>
2023-06-19 06:54:54 -0700

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *  *  *  *  * main - void
+ *   *   *   * Return: Always 0 (Success)
+ *    *    *    **/
+
+int main(void)
+{
+	int number;
+
+	number = 0;
+
+
+	for (number = 0; number <= 9; number++)
+	{
+
+		putchar(number);
+
+	}
+	putchar('\n');
+	return (0);
+}
