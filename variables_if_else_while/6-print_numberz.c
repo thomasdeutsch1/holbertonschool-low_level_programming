803454382a38bdec03ba0330d17b438380520438
Thomas <thomasdeutschpro@gmail.com>
2023-06-19 06:56:10 -0700

    6-print_numberz.c

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	int number;
 
-	number = 0;
+	number = '0';
 
 
-	for (number = 0; number <= 9; number++)
+	for (number = '0'; number <= '9'; number++)
 	{
 
 		putchar(number);
