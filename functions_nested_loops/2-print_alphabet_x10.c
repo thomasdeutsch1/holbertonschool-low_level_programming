c4c82bf703397a752cf451dbc94a0f1cc1d39672
Thomas <thomasdeutschpro@gmail.com>
2023-06-20 02:29:26 -0700

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ *  *print_alphabet - void
+ *   **/
+
+void print_alphabet_x10(void);
+
+
+	char lettre;
+
+	for (int i = 0; i < 10; i++)
+	{
+
+	for (lettre = 'a'; lettre <= 'z'; lettre++)
+
+	{_putchar(lettre); }
+
+	_putchar('\n');
+
