52d577bb58e5de080c50dddbbd8d65886f201de3
Thomas <thomasdeutschpro@gmail.com>
2023-06-21 01:36:35 -0700

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -14,10 +14,11 @@ int print_sign(int n)
 	if (n > '0')
 
 	{
-		return (1);
+	return (1);
 	}
+	
 
-	else if (n = '0')
+	else if (n == '0')
 
 	{
 		return (0);
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
-int _putchar(char c);
+#include <unistd.h>
+int _putchar(char c)
+{
+		return (write(1, &c, 1));
+}
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
