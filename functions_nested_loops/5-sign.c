cf2cfc67488aa986a5ce34417a6e6e73560239d3
Thomas <thomasdeutschpro@gmail.com>
2023-06-21 01:02:14 -0700

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *_islower - if else
+ * @c: int
+ ** Description: islower
+ ** Return: Descrition
+ **/
+
+
+int print_sign(int n)
+
+{
+	if(n > '0')
+
+	{
+		return (1);
+	}
+
+	else if(n = '0')
+
+	{
+		return (0);
+	}
+
+	
+	else if(n < '0')
+	
+	{	
+		return (-1);
+	}	
+}
