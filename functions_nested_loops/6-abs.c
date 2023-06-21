98af4950088ebce3e84fadbc31387e6eb7f112f7
Thomas <thomasdeutschpro@gmail.com>
2023-06-21 04:40:16 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int _abs
+ *  @c: int
+ * Description: int_abs
+ * Return: n
+ **/
+
+
+int _abs(int n)
+
+{
+	if (n < 0)
+	{ return (n * -1);
+	}
+	else
+	{ return (n);
+	}
+}
