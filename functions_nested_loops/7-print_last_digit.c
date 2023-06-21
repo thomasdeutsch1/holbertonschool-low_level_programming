fcf2c8fa78206b7b6b2c418ea8a48a5e6e93f1c2
Thomas <thomasdeutschpro@gmail.com>
2023-06-21 05:03:11 -0700

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  *  *  *  * _isalpha - if else
+ *   *   *   *   * @c: int
+ *    *    *    *    * Description: _isalpha
+ *     *     *     *     * Return: Descrition
+ *      *      *      *      */
+
+
+int print_last_digit(int r)
+
+
+{
+	int last_digit = r % 10;
+
+	if (last_digit < 0)
+	{	last_digit *= -1;
+	}
+	{
+	return (r); }
+}
