f0f1817efb4ad055056289e744ba7cc32c378275
Thomas <thomasdeutschpro@gmail.com>
2023-06-21 05:49:11 -0700

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -14,16 +14,15 @@ int print_last_digit(int number)
 	int last;
 
 	if (number < 0)
-	{	
+	{
 		last = number % 10 * -1;
 		_putchar(last + '0');
-		return(last);
+		return (last);
 	}
 	else
 	{
 		last = number % 10;
 		_putchar(last + '0');
-		return (last); 
+		return (last);
 	}
-
 }
