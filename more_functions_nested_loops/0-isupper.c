bf6052b00752099c5ba90c0779ebca282b97a4ec
Thomas <thomasdeutschpro@gmail.com>
2023-06-22 14:11:06 -0700

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 int _isupper(int c)
-
 {
 	if (c >= 'A' && c <= 'Z')
-
-		return (1);
-
+		return 1;
 	else
+		return 0;
+}
 
-		return (0);
+int main(void)
+{
+	_putchar('\n');
 
-_putchar('\n');
+	return 0;
 }
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,11 +3,7 @@
 
 #include <unistd.h>
 
-int _putchar(char c)
-{
-		return (write(1, &c, 1));
-}
-
+int _putchar(char c);
 int _isupper(int c);
 
 
