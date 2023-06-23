164f57e73bb41a831d1d3eecd511cbfe965d9daa
Thomas <thomasdeutschpro@gmail.com>
2023-06-23 05:33:23 -0700

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,7 @@
 #include "main.h"
+/**
+ * more_numbers - ok
+ */
 
 void more_numbers(void)
 {
@@ -7,12 +10,12 @@ void more_numbers(void)
 
 	for (b = 0; b < 10; b++)
 
-	{ 
-		for(c = 0; c <= 14; c++)
+	{
+		for (c = 0; c <= 14; c++)
 		{
 			if (c > 9)
 			{
-			_putchar((c / 10 ) + '0');
+			_putchar((c / 10) + '0');
 			}
 			_putchar((c % 10) + '0');
 
