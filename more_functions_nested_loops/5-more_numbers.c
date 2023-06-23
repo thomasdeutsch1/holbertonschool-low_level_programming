bb115967d347a323741013f3637532a6f59eff68
Thomas <thomasdeutschpro@gmail.com>
2023-06-23 05:06:16 -0700

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+void more_numbers(void)
+{
+	int b;
+	int c;
+
+	for (b = 0; b < 10; b++)
+
+	{ 
+		for(c = 0; c <= 14; c++)
+		{
+			if (c => 10)
+			{
+			c = c / 10;
+			_putchar(c+'0');
+			}
+			_putchar(c % 10 + '0');
+
+		}
+
+		_putchar('\n');
+	}
+}
