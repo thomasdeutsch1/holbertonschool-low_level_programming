53b85c38bc0835e567268dd3704fd7c84d4a7797
Thomas <thomasdeutschpro@gmail.com>
2023-06-26 02:35:57 -0700

    1-swap.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /*
- * Description: function that takes a pointer to an int
+ * Description: reset_to_98
  *
  */
 
---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+void swap_int(int *a, int *b)
+
+{ int x;
+	
+	x = 0;
+	x = *a;
+	*a = *b;
+	*b = x;
+
+}
